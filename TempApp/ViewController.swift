9a7e37db5def30f90355bf14813a2daf3daec275
Wade Sellers <wade@wadesellers.com>
2023-06-23 12:58:11 -0400

    ability to automatically zoom in to current location

--- TempApp/ViewController.swift
+++ TempApp/ViewController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+    
+    //mapView delegate functions
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.first!
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        self.mapView.setRegion(region, animated: true)
     }
 
 
