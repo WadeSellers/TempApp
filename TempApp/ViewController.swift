3e3f4d8107906a108a87423102fbeae2a01c0ac6
Wade Sellers <wade@wadesellers.com>
2023-06-23 12:52:38 -0400

    ability to display current location on a map

--- TempApp/ViewController.swift
+++ TempApp/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
